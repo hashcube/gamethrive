69f0e5dd830a80ad94a088f01c7acf7db19fd924
rohan92 <rohan@hashcube.com>
2015-03-16 20:25:57 +0530

    Slight changes to BroadcastReceiver

--- android/GameBroadcastReceiver.java
+++ android/GameBroadcastReceiver.java
@@ -1,8 +1,6 @@
 package com.gamethrive;
 
 import com.tealeaf.logger;
-import com.tealeaf.EventQueue;
-import com.tealeaf.event.Event;
 
 import android.content.BroadcastReceiver;
 import android.content.Intent;
@@ -14,12 +12,6 @@ public class GameBroadcastReceiver extends BroadcastReceiver {
   private static Date received_on_time = null;
   private static Integer received_count = 0;
   private static final String TAG = "{{GameThrivePlugin}}";
-
-  //Singleton implementation
-  //public final static GameBroadcastReceiver INSTANCE = new GameBroadcastReceiver();
-  // private GameBroadcastReceiver() {
-    // Exists only to defeat instantiation.
-  //}
 
   // You may consider adding a wake lock here if you need to make sure the devices doesn't go to sleep while processing.
   // We recommend starting a service of your own here if your doing any async calls or doing any heavy processing.
